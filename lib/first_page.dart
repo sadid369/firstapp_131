c94855963014d16f18e37814753e48dd57a3ea9e
Sadid <sadid.jones@gmail.com>
2023-06-08 22:41:56 +0600

    555

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -6,18 +6,41 @@ class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('My App'),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              Text("Hello, Welcome to Flutter"),
-              TextField(
-                decoration: InputDecoration(border: InputBorder.none),
+      appBar: AppBar(
+        title: const Text('My App'),
+      ),
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          child: Center(child: Text('Hellommm')),
+          decoration: BoxDecoration(
+            color: Color(0xFFECDC00),
+            borderRadius: BorderRadius.circular(15.0),
+            // border: Border.all(
+            //   color: Color(0x8CB92929),
+            //   width: 34,
+            //   strokeAlign: BorderSide.strokeAlignCenter,
+            // ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.shade500,
+                blurRadius: 5,
+                spreadRadius: 1,
+                offset: const Offset(0, 5),
+              ),
+              BoxShadow(
+                color: Colors.grey.shade500,
+                offset: const Offset(0, -5),
+              ),
+              BoxShadow(
+                color: Colors.grey.shade500,
+                offset: const Offset(-5, 0),
               ),
             ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
