0bf534273f6c692b5d118ff08f0225b14f78ad60
sadid369 <sadid.jones@gmail.com>
2023-07-11 22:46:22 +0600

    updated

--- lib/animation_controller_exp.dart
+++ lib/animation_controller_exp.dart
@@ -8,10 +8,14 @@ class AnimationControllerExp extends StatefulWidget {
 }
 
 class _AnimationControllerExpState extends State<AnimationControllerExp>
-    with SingleTickerProviderStateMixin {
+    with TickerProviderStateMixin {
   late AnimationController mController;
+  late AnimationController mSecController;
   late Animation tweenAnim;
   late Animation colorAnim;
+  late Animation alineAnim;
+  late Animation<Size?> sizeAnim;
+
   @override
   void initState() {
     super.initState();
@@ -19,48 +23,56 @@ class _AnimationControllerExpState extends State<AnimationControllerExp>
       vsync: this,
       duration: Duration(seconds: 5),
     );
-    // var curve = CurvedAnimation(parent: mController, curve: Curves.bounceOut);
+    mSecController =
+        AnimationController(vsync: this, duration: Duration(seconds: 5))
+          ..addListener(() {
+            setState(() {});
+          });
+
     tweenAnim = Tween(
       begin: 11.0,
-      end: 20.0,
+      end: 60.0,
     ).animate(CurvedAnimation(
-      parent: mController,
+      parent: mSecController,
       curve: Curves.bounceInOut,
     ));
-    colorAnim =
-        ColorTween(begin: Colors.amber, end: Colors.blue).animate(mController);
+    colorAnim = ColorTween(begin: Colors.amber, end: Colors.blue)
+        .animate(mSecController);
+    alineAnim =
+        AlignmentTween(begin: Alignment.bottomLeft, end: Alignment.topRight)
+            .animate(mController);
+    sizeAnim = SizeTween(begin: Size(200, 100), end: Size(100, 200))
+        .animate(mController);
     mController.addListener(() {
-      print(mController.value);
-      print(tweenAnim.value);
+      mController.addStatusListener(
+        (status) {
+          if (status == AnimationStatus.completed) {
+            mSecController.forward();
+          }
+        },
+      );
       setState(() {});
     });
-    // mController.forward();
+    mController.forward();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // backgroundColor: colorAnim.value,
       appBar: AppBar(
         title: const Text('Controller'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              width: 300 * mController.value,
-              height: 300 * mController.value,
-              decoration: BoxDecoration(
-                  color: colorAnim.value,
-                  borderRadius: BorderRadius.circular(tweenAnim.value)),
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  mController.forward();
-                },
-                child: Text('Tap Me'))
-          ],
+      body: Align(
+        alignment: alineAnim.value,
+        child: Container(
+          width: sizeAnim.value!.width,
+          height: sizeAnim.value!.height,
+          child: Icon(Icons.account_balance_outlined, size: tweenAnim.value),
+          // width: 300 * mController.value,
+          // height: 300 * mController.value,
+          decoration: BoxDecoration(
+              color: colorAnim.value, borderRadius: BorderRadius.circular(15)),
         ),
       ),
     );
