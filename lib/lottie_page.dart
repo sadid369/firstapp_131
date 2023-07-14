5dd6dc30983cde1bec69a2e02637758bc70dc545
sadid369 <sadid.jones@gmail.com>
2023-07-14 23:01:46 +0600

    555

--- lib/lottie_page.dart
+++ lib/lottie_page.dart
@@ -8,27 +8,45 @@ class LottiePage extends StatefulWidget {
   _LottiePageState createState() => _LottiePageState();
 }
 
-class _LottiePageState extends State<LottiePage> {
-  var isVisisble = true;
+class _LottiePageState extends State<LottiePage> with TickerProviderStateMixin {
+  late AnimationController _mController;
+  @override
+  void initState() {
+    super.initState();
+    _mController = AnimationController(vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Lottie Animation'),
       ),
-      body: Center(
-          child: InkWell(
-        onTap: () {
-          isVisisble = !isVisisble;
-          setState(() {});
-        },
-        child: SizedBox(
-            height: 200,
-            width: 200,
-            child: Visibility(
-                visible: isVisisble,
-                child: Lottie.asset('assets/lottie/confirm_animation.json'))),
-      )),
+      body: Column(
+        children: [
+          SizedBox(
+              height: 300,
+              width: 400,
+              child: Lottie.asset(
+                'assets/lottie/confirm_animation.json',
+                controller: _mController,
+                onLoaded: (composition) {
+                  _mController.duration = composition.duration;
+                },
+              )),
+          ElevatedButton(
+            onPressed: () {
+              if (_mController.isAnimating) {
+                _mController.stop();
+              } else {
+                _mController.repeat(reverse: true);
+              }
+              setState(() {});
+            },
+            child: Text(_mController.isAnimating ? 'Playing' : "Play"),
+          )
+        ],
+      ),
     );
   }
 }
