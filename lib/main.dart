ac676e8255aea0ef2e717be0d6f285e4bbf581f3
sadid369 <sadid.jones@gmail.com>
2023-07-15 22:35:27 +0600

    rive

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import 'package:firstapp_131/images.dart';
 import 'package:firstapp_131/list_view.dart';
 import 'package:firstapp_131/lottie_page.dart';
 import 'package:firstapp_131/page_transiction_exp.dart';
+import 'package:firstapp_131/rive_animation_page.dart';
 import 'package:firstapp_131/shrink_warp.dart';
 import 'package:firstapp_131/splash_page.dart';
 import 'package:firstapp_131/stack_widget.dart';
@@ -36,7 +37,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LottiePage(),
+      home: const RiveAnimationPage(),
     );
   }
 }
---
+++ lib/rive_animation_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:rive/rive.dart';
+
+class RiveAnimationPage extends StatefulWidget {
+  const RiveAnimationPage({Key? key}) : super(key: key);
+
+  @override
+  _RiveAnimationPageState createState() => _RiveAnimationPageState();
+}
+
+class _RiveAnimationPageState extends State<RiveAnimationPage> {
+  var rivePath = "assets/rive/star.riv";
+  StateMachineController? machineController;
+  Artboard? mainArtBoard;
+  SMIInput<bool>? riveInput;
+
+  @override
+  void initState() {
+    super.initState();
+
+    rootBundle.load(rivePath).then((riveByteData) {
+      var file = RiveFile.import(riveByteData);
+
+      var artBoard = file.mainArtboard;
+      machineController =
+          StateMachineController.fromArtboard(artBoard, "starFav");
+      if (machineController != null) {
+        artBoard.addController(machineController!);
+        riveInput = machineController!.findInput('check');
+
+        mainArtBoard = artBoard;
+        setState(() {});
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Rive Animation'),
+        ),
+        body: mainArtBoard == null
+            ? Container()
+            : InkWell(
+                onTap: () {
+                  if (riveInput != null) {
+                    if (riveInput!.value == false &&
+                        riveInput!.controller.isActive == false) {
+                      riveInput!.value = true;
+                    } else if (riveInput!.value == true &&
+                        riveInput!.controller.isActive == true) {
+                      riveInput!.value = false;
+                    }
+                    // if (riveInput != null) {
+                    //   if (riveInput!.value == false &&
+                    //       riveInput!.controller.isActive == false) {
+                    //     riveInput!.value = true;
+                    //   } else {
+                    //     riveInput!.value = false;
+                    //   }
+                    // }
+                  }
+                },
+                child: Rive(artboard: mainArtBoard!)));
+    // body: Container());
+  }
+}
